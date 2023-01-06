59c5af718fb8ae526d3e774e9101bb99b61489f4
annafsullivan <113389936+annafsullivan@users.noreply.github.com>
2023-01-06 09:55:16 -0500

    2x2 grid

--- CreativeCoding/background/sketch.js
+++ CreativeCoding/background/sketch.js
@@ -1,7 +1,10 @@
-function setup() {
-
+function setup() {//runs once
+createCanvas (720,720);//create a canvas 720 pixel by 720 pixels
 }
 
-function draw() {
-
+function draw() {//runs in a loop
+quad (0,0,
+      300,0,
+      300,300,
+      0,300);
 }
