d9acafd6c2b193bf69a173ad4158dc439d545240
annafsullivan <113389936+annafsullivan@users.noreply.github.com>
2023-01-09 12:52:40 -0500

    tune variables

---
+++ CreativeCoding/CreativeCoding/TuneVariables/sketch.js
@@ -0,0 +1,24 @@
+//create a progam that enables us to customize the number of squares in a grid
+
+function setup() {//runs once
+createCanvas (windowWidth, windowWidth);//creates a canvas 720 pixels by 720 pixels
+background ('maroon');//background color
+}
+
+function draw() {//runs in loop
+fill (255,0,0);//fills shape with RGB color
+strokeWeight (1);//assigns stroke weight
+
+var num = 2; //number of sides in the array
+var sideLen = windowWidth/num; //side length variable
+
+
+for (var y = 0; y < windowWidth; y = y + sideLen){
+  for (var x = 0; x < windowWidth; x = x + sideLen){
+    quad (x,y,
+          x + sideLen,y,
+          x + sideLen,y + sideLen,
+          x,y + sideLen);
+  }
+}
+}
