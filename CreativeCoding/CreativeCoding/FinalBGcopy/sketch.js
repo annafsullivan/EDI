424b9f94aa3bae7f499bfdda5a90e3720ca99194
annafsullivan <113389936+annafsullivan@users.noreply.github.com>
2023-01-13 11:43:07 -0500

    fix

--- CreativeCoding/CreativeCoding/FinalBGcopy/sketch.js
+++ CreativeCoding/CreativeCoding/FinalBGcopy/sketch.js
@@ -9,12 +9,12 @@ function preload () {
 
 function setup() {//runs once
 createCanvas (windowWidth, windowWidth);//creates a canvas 720 pixels by 720 pixels
-background ('225, 250, 252');//background color
+background (225, 250, 252);//background color
 }
 
 function draw() {//runs in loop
-fill ('turquoise');//fills shape with RGB color
-strokeWeight (1);//assigns stroke weight
+//fill ('turquoise');//fills shape with RGB color
+//strokeWeight (1);//assigns stroke weight
 
 var num = 2; //number of sides in the array
 var sideLen = windowWidth/num; //side length variable
@@ -23,6 +23,9 @@ var sideLen = windowWidth/num; //side length variable
 for (var y = 0; y < windowWidth; y = y + sideLen){
   for (var x = 0; x < windowWidth; x = x + sideLen){
 image (gif,x,y,windowWidth/num, windowWidth/num);
+  }
+  function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
   }
 }
 }
